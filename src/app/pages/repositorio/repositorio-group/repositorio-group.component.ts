d9431d262591f776e10b8e9b66896d55448f3493
Teresa Margarita Orozco Bon <torozcobon@guanajuato.gob.mx>
2020-05-24 21:17:26 -0500

    ADD configuracion; ADD grupo de repositorios

---
+++ src/app/pages/repositorio/repositorio-group/repositorio-group.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repositorio-group',
+  templateUrl: './repositorio-group.component.html',
+  styleUrls: ['./repositorio-group.component.css']
+})
+export class RepositorioGroupComponent implements OnInit {
+  focus: any;
+  focus1: any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
